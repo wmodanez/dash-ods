ecfeecd2cd06ef27d3560b2f030a36114a8598f5
Wesley Modanez Freitas <wesley.modanez@gmail.com>
2025-03-27 10:37:28 -0300

    Modificar configuração de cache do servidor para desativar o cache de arquivos estáticos, alterando o valor de 'SEND_FILE_MAX_AGE_DEFAULT' para -1.

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ DASH_CONFIG = {}
 
 # Configurações de cache do servidor
 SERVER_CONFIG = {
-    'SEND_FILE_MAX_AGE_DEFAULT': 31536000,  # Cache de 1 ano para arquivos estáticos
+    'SEND_FILE_MAX_AGE_DEFAULT': -1,  # Cache de 1 ano para arquivos estáticos
     'PERMANENT_SESSION_LIFETIME': 1800,  # 30 minutos para sessões
     'STATIC_FOLDER': 'db/resultados',  # Pasta onde estão os arquivos parquet
     'STATIC_URL_PATH': '/db/resultados',  # URL path para acessar os arquivos
